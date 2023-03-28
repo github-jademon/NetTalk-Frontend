0af40fcda1ebdd85a94d6fd99b4899cbb4192de6
jademon <j2python@gmail.com>
2023-03-28 17:25:58 +0900

    add get data to server

--- src/pages/Main.js
+++ src/pages/Main.js
@@ -1,53 +1,20 @@
-import React, { useState } from "react";
+import axios from "axios";
+import React, { useEffect, useState } from "react";
 import { Link } from "react-router-dom";
 
-const data = [
-  {
-    id: 1,
-    title: "title1",
-    userCount: 1,
-    maxCount: 10,
-    comment: "comment1",
-  },
-  {
-    id: 2,
-    title: "title2",
-    userCount: 2,
-    maxCount: 10,
-    comment: "comment2",
-  },
-  {
-    id: 3,
-    title: "title3",
-    userCount: 3,
-    maxCount: 10,
-    comment: "comment3",
-  },
-  {
-    id: 4,
-    title: "title4",
-    userCount: 4,
-    maxCount: 10,
-    comment: "comment4",
-  },
-  {
-    id: 5,
-    title: "title5",
-    userCount: 5,
-    maxCount: 10,
-    comment: "comment5",
-  },
-  {
-    id: 6,
-    title: "title6",
-    userCount: 6,
-    maxCount: 10,
-    comment: "comment6",
-  },
-];
-
 const Main = (props) => {
-  // const [data, setData] = useState([]);
+  const [data, setData] = useState([]);
+
+  useEffect(() => {
+    loadData();
+  }, []);
+
+  const loadData = async () => {
+    const response = await axios
+      .get("/api/rooms")
+      .catch((err) => console.log(err));
+    setData(response.data);
+  };
 
   return (
     <div className="container">
