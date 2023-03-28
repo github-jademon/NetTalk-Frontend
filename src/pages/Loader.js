e091dadf2281083d0e9b117111b4363e8e38caea
jademon <j2python@gmail.com>
2023-03-28 20:30:13 +0900

    add loader

---
+++ src/pages/Loader.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Loader = () => {
+  return (
+    <div class="loader-container">
+      <div className="loader"></div>
+    </div>
+  );
+};
+
+export default Loader;
