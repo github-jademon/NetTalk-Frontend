a61102bb2b914319d5df918251e38d0db7940831
jademon <j2python@gmail.com>
2023-03-28 16:13:52 +0900

    add room list

--- src/pages/Main.js
+++ src/pages/Main.js
@@ -1,18 +1,66 @@
-import React from "react";
+import React, { useState } from "react";
 import { Link } from "react-router-dom";
 
+const data = [
+  {
+    id: 1,
+    title: "title1",
+    userCount: 1,
+    maxCount: 10,
+    comment: "comment1",
+  },
+  {
+    id: 2,
+    title: "title2",
+    userCount: 2,
+    maxCount: 10,
+    comment: "comment2",
+  },
+  {
+    id: 3,
+    title: "title3",
+    userCount: 3,
+    maxCount: 10,
+    comment: "comment3",
+  },
+  {
+    id: 4,
+    title: "title4",
+    userCount: 4,
+    maxCount: 10,
+    comment: "comment4",
+  },
+  {
+    id: 5,
+    title: "title5",
+    userCount: 5,
+    maxCount: 10,
+    comment: "comment5",
+  },
+  {
+    id: 6,
+    title: "title6",
+    userCount: 6,
+    maxCount: 10,
+    comment: "comment6",
+  },
+];
+
 const Main = (props) => {
+  // const [data, setData] = useState([]);
+
   return (
     <div className="container">
       <h3>안녕하세요. 메인페이지 입니다.</h3>
-      <ul>
-        <Link to="/product/1">
-          <li>1번상품</li>
+      {data.map((v, i) => (
+        <Link to={"/room/" + v.id} key={i} className="room">
+          <div>{i + 1}</div>
+          <div>{v.title}</div>
+          <div>{v.userCount}</div>
+          <div>{v.maxCount}</div>
+          <div>{v.comment}</div>
         </Link>
-        <Link to="/product/2">
-          <li>2번상품</li>
-        </Link>
-      </ul>
+      ))}
     </div>
   );
 };
