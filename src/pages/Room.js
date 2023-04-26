25f269e9219009edde6f9b5e8ac06c9c9401a85f
jademon <j2python@gmail.com>
2023-04-26 19:19:25 +0900

    [FEAT] room change based on token value

--- src/pages/Room.js
+++ src/pages/Room.js
@@ -1,12 +1,115 @@
-import React, { useState, useEffect } from "react";
+import React, { useState, useEffect, useRef } from "react";
+import * as StompJs from "@stomp/stompjs";
+import * as SockJS from "sockjs-client";
 import { useParams } from "react-router";
-import { Loader, Socket } from "pages";
+import { Loader } from "pages";
 import axios from "axios";
+import { setInterceptor } from "assets/setInterceptor";
 
 const Room = () => {
-  const [data, setData] = useState();
   const params = useParams();
-  const id = params.id;
+  const roomId = params.id;
+  const client = useRef({});
+  const scrollRef = useRef();
+  const [data, setData] = useState();
+  const [name, setName] = useState("");
+  const [message, setMessage] = useState("");
+  const [messages, setMessages] = useState([]);
+  const [chatMessages, setChatMessages] = useState();
+  const [check, setCheck] = useState(false);
+  const [socket, setSocket] = useState(false);
+  const [uuid, setUuid] = useState();
+
+  useEffect(() => {
+    if (uuid == null) {
+      return;
+    }
+    connect();
+
+    return () => disconnect();
+  }, [uuid]);
+
+  useEffect(() => {
+    console.log(messages);
+    if (check) {
+      try {
+        space();
+      } catch (e) {
+        console.log(e);
+      }
+    }
+  }, [messages]);
+
+  useEffect(() => {
+    if (!chatMessages) return;
+    setMessages(chatMessages);
+  }, [chatMessages]);
+
+  const connect = () => {
+    if (uuid == null) {
+      return;
+    }
+
+    client.current = new StompJs.Client({
+      webSocketFactory: () => new SockJS("/stomp/chat"),
+      debug: function (str) {
+        console.log(str);
+      },
+      onConnect: () => {
+        setSocket(true);
+        if (chatMessages) setCheck(true);
+        client.current.subscribe(
+          `/sub/chat/room/${roomId}`,
+          async ({ body }) => {
+            console.log(body);
+            setMessages((_chatMessages) => [
+              ..._chatMessages,
+              JSON.parse(body),
+            ]);
+            console.log(messages);
+          }
+        );
+      },
+      connectHeaders: {
+        Authorization: `Bearer ${localStorage.getItem("token")}`,
+      },
+      onStompError: (frame) => {
+        console.error(frame);
+      },
+    });
+
+    client.current.activate();
+  };
+
+  const disconnect = () => {
+    client.current.deactivate();
+  };
+
+  const send = () => {
+    if (!client.current.connected) {
+      return;
+    }
+
+    if (message === "") {
+      alert("메세지를 입력하세요.");
+      document.getElementById("message").focus();
+      return;
+    } else {
+      client.current.publish({
+        destination: "/pub/chat/message",
+        body: JSON.stringify({
+          roomId: roomId,
+          type: "user",
+          username: name,
+          uuid: uuid,
+          message,
+          date: new Date().toLocaleString(),
+        }),
+      });
+
+      setMessage("");
+    }
+  };
 
   useEffect(() => {
     if (!data) {
@@ -15,12 +118,68 @@ const Room = () => {
   }, []);
 
   const loadData = async () => {
+    const token = localStorage.getItem("token");
+
+    setInterceptor(token);
+
     const response = await axios
-      .get("/api/rooms/" + id)
+      .get(`/api/rooms/${roomId}`)
       .catch((err) => console.log(err));
+
     console.log(response.data);
-    setData(response.data);
+
+    setData(response.data.room);
+    setChatMessages(response.data.messages);
+
+    if (response.data.user) {
+      setUuid(response.data.user.email);
+      setName(response.data.user.name);
+    }
+
+    if (response.data.user == null) {
+      localStorage.setItem("uuid", uuidv4());
+      setUuid(localStorage.getItem("uuid"));
+    }
   };
+
+  const onOpen = async () => {
+    if (!client.current.connected) {
+      return;
+    }
+
+    client.current.publish({
+      destination: "/pub/chat/enter",
+      body: JSON.stringify({
+        roomId: roomId,
+        type: "system",
+        name: name,
+      }),
+    });
+
+    const body = {
+      email: uuid,
+      name: name,
+    };
+
+    axios
+      .post(`/api/rooms/${roomId}/name`, body)
+      .catch((err) => console.log(err));
+
+    setCheck(true);
+  };
+
+  const space = () => {
+    scrollRef.current.scrollTop = scrollRef.current.scrollHeight;
+  };
+
+  function uuidv4() {
+    return ([1e7] + -1e3 + -4e3 + -8e3 + -1e11).replace(/[018]/g, (c) =>
+      (
+        c ^
+        (crypto.getRandomValues(new Uint8Array(1))[0] & (15 >> (c / 4)))
+      ).toString(16)
+    );
+  }
 
   return data ? (
     <div className="container room">
@@ -29,7 +188,76 @@ const Room = () => {
       </div>
       <div>{data.comment}</div>
       <hr />
-      <Socket roomId={id} />
+      {!check ? (
+        <div className="input-modal">
+          {!socket || chatMessages ? (
+            <Loader />
+          ) : (
+            <div className="input-name-container">
+              <div className="title">이름!</div>
+              <div className="input-name">
+                <input
+                  type="text"
+                  id="message"
+                  className="input-text"
+                  value={name}
+                  onChange={(e) => setName(e.target.value)}
+                  onKeyDown={(ev) => {
+                    if (ev.keyCode === 13) {
+                      onOpen();
+                    }
+                  }}
+                />
+                <input type="button" value="확인" onClick={onOpen} />
+              </div>
+            </div>
+          )}
+        </div>
+      ) : (
+        <>
+          <div className="talk room" ref={scrollRef}>
+            {messages ? (
+              <>
+                {messages.map((v, i) => (
+                  <div
+                    key={i}
+                    className={
+                      v.uuid === uuid
+                        ? "user"
+                        : v.type === "system"
+                        ? "system"
+                        : "partner"
+                    }
+                  >
+                    {v.type === "user" ? (
+                      <div className="info">{v.username}</div>
+                    ) : null}
+                    <div className="text">{v.message}</div>
+                    {v.type === "system" ? null : (
+                      <div className="date">{v.date}</div>
+                    )}
+                  </div>
+                ))}
+              </>
+            ) : null}
+          </div>
+          <div className="input">
+            <input
+              type="text"
+              id="message"
+              className="input-text"
+              value={message}
+              onChange={(e) => setMessage(e.target.value)}
+              onKeyDown={(ev) => {
+                if (ev.keyCode === 13) {
+                  send();
+                }
+              }}
+            ></input>
+            <input type="button" value="보내기" onClick={send} />
+          </div>
+        </>
+      )}
     </div>
   ) : (
     <Loader />
