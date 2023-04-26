3d773b65143fd8df13bed9226a15ecadb24f0530
jademon <j2python@gmail.com>
2023-04-26 19:16:16 +0900

    [FEAT] header design change based on token value

--- src/pages/Header.js
+++ src/pages/Header.js
@@ -12,7 +12,48 @@ const Header = () => {
     fontSize: "1rem",
   };
 
-  return (
+  const token = localStorage.getItem("token");
+
+  const logout = () => {
+    localStorage.removeItem("token");
+    window.location.reload();
+  };
+
+  return token ? (
+    <div className="header">
+      <ul>
+        <li>
+          <NavLink
+            to="/"
+            style={({ isActive }) => (isActive ? activeStyle : defaultStyle)}
+          >
+            홈
+          </NavLink>
+        </li>
+        <li>
+          <NavLink
+            to="/mypage"
+            style={({ isActive }) => (isActive ? activeStyle : defaultStyle)}
+          >
+            마이페이지
+          </NavLink>
+        </li>
+        <li>
+          <NavLink
+            to="/createRoom"
+            style={({ isActive }) => (isActive ? activeStyle : defaultStyle)}
+          >
+            채팅방생성
+          </NavLink>
+        </li>
+        <li>
+          <div className="logout" onClick={() => logout()}>
+            로그아웃
+          </div>
+        </li>
+      </ul>
+    </div>
+  ) : (
     <div className="header">
       <ul>
         <li>
