dd740f2a302aa43e00a55ee23ebe6994afd1c2ec
jademon <j2python@gmail.com>
2023-04-26 18:10:08 +0900

    [ADD] add route mypage, createRoom

--- src/App.js
+++ src/App.js
@@ -1,9 +1,15 @@
-import React from "react";
+import React, { useEffect } from "react";
 import "./App.css";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
-import { Header, Main, Room, SignUp, NotFound, SignIn } from "pages";
+import { Header, Main, Room, SignUp, NotFound, SignIn, Mypage } from "pages";
+import CreateRoom from "pages/CreateRoom";
 
 const App = () => {
+  useEffect(() => {
+    if (localStorage.getItem("token") === null) {
+    }
+  }, []);
+
   return (
     <>
       <BrowserRouter>
@@ -13,6 +19,8 @@ const App = () => {
             <Route path="/" element={<Main />}></Route>
             <Route path="/signin" element={<SignIn />}></Route>
             <Route path="/signup" element={<SignUp />}></Route>
+            <Route path="/mypage" element={<Mypage />}></Route>
+            <Route path="/createRoom" element={<CreateRoom />}></Route>
             <Route path="/room/:id" element={<Room />}></Route>
             <Route path="*" element={<NotFound />}></Route>
           </Routes>
